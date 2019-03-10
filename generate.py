8eaa28782b2139ed9a7b1c297d237d82fd866dce
mzgubic <miha.zgubic@gmail.com>
2019-03-10 11:39:28 +0000

    expand to other distributions

--- generate.py
+++ generate.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def triangular1D(N):
-    return np.random.triangular(-1, 1, 1, N).reshape(-1, 1)
+    return np.random.triangular(0, 1, 1, N).reshape(-1, 1)
+
+def uniform1D(N):
+    return np.random.uniform(0, 1, N).reshape(-1, 1)
+
+def beta1D(N):
+    return np.random.beta(2, 3, N).reshape(-1, 1)
 
 def input_noise(noise_dim=3):
     def f(N):
--- plot.py
+++ plot.py
@@ -1,15 +1,31 @@
+import numpy as np
 import sklearn.metrics
 from sklearn.metrics import roc_auc_score
+
 
 def data1D(ax, real_data, fake_data):
     
     bins = 30
-    xlo, xhi = -1.5, 1.5
+    xlo, xhi = -0.25, 1.25
     _ = ax.hist(real_data, bins=bins, alpha=0.7, range=(xlo, xhi), label='real data')
     _ = ax.hist(fake_data, bins=bins, alpha=0.7, range=(xlo, xhi), label='fake data')
     _ = ax.set_xlim(xlo, xhi)
     _ = ax.legend(loc='best')
-    _ = ax.set_xlabel('False positive rate')
+    ax.set_xlim(xlo, xhi)
+
+
+def ratio1D(ax, real_data, fake_data):
+
+    bins = 30
+    xlo, xhi = -0.25, 1.25
+    rh, edges = np.histogram(real_data, bins=bins, range=(xlo, xhi))
+    fh, edges = np.histogram(fake_data, bins=bins, range=(xlo, xhi))
+    upper, lower = edges[1:], edges[:-1]
+    centres = 0.5*(upper+lower)
+    _ = ax.hist(centres, weights=rh/fh, color='k', histtype='step', bins=bins, range=(xlo, xhi))
+    _ = ax.plot([0, 1], [1.0, 1.0], 'r')
+    _ = ax.set_xlabel('modelled variable')
+    ax.set_xlim(xlo, xhi)
 
 
 def roc_curve(ax, labels, logits):
